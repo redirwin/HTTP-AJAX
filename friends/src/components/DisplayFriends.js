8d6a4e456055710de930c0956f95f1f5cd90ed81
Dave Irwin <david@redirwin.com>
2019-06-12 13:46:14 -0700

    Created DisplayFriends, imported to App, and added Route to display it on root.

---
+++ friends/src/components/DisplayFriends.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route } from "react-router-dom";
+
+class DisplayFriends extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>These are my friends.</h1>
+        <h2>They are pretty cool.</h2>
+      </div>
+    );
+  }
+}
+
+export default DisplayFriends;
