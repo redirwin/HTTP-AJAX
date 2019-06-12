a5e0977ac21e9a9121f550f8851b89afd9ca5faf
Dave Irwin <david@redirwin.com>
2019-06-12 14:02:04 -0700

    Successfully getting data from server and displaying.

--- friends/src/components/DisplayFriends.js
+++ friends/src/components/DisplayFriends.js
@@ -1,12 +1,36 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import axios from "axios";
 
 class DisplayFriends extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/friends")
+      .then(response => {
+        this.setState(() => ({ friends: response.data }));
+      })
+      .catch(error => {
+        console.error("Server Error", error);
+      });
+  }
+
   render() {
+    console.log(this.state.friends);
     return (
-      <div>
-        <h1>These are my friends.</h1>
-        <h2>They are pretty cool.</h2>
+      <div className="friends-list">
+        {this.state.friends.map(friend => (
+          <div key={friend.id} className="friend-details">
+            <h2>{friend.name}</h2>
+            <p>age: {friend.age}</p>
+            <p>email: {friend.email}</p>
+          </div>
+        ))}
       </div>
     );
   }
