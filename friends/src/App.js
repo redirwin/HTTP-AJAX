e411369ee87d548843026961b9585ef9a306177f
Dave Irwin <david@redirwin.com>
2019-06-12 14:41:04 -0700

    Created AddFriend and Header components.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { Route } from "react-router-dom";
 
+import Header from "./components/Header";
 import DisplayFriends from "./components/DisplayFriends";
+import AddFriend from "./components/AddFriend";
 import "./App.scss";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Route exact path="/" component={DisplayFriends} />
+        <Route path="/addfriend" component={AddFriend} />
       </div>
     );
   }
---
+++ friends/src/components/AddFriend.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+class AddFriends extends React.Component {}
+
+export default AddFriends;
